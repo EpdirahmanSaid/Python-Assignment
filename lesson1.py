23686ef7899ccdd5b3dafcdaca1f5041a96f4a8b
EpdirahmanSaid <epdirahman00@gmail.com>
2026-07-27 19:07:05 +0300

    Assignment Variables

---
+++ lesson1.py
@@ -0,0 +1,38 @@
+"""Name , Age , Gender = "Updallahi" , 22 , "Male"
+print(Name)
+print(Age)
+print (Gender)"""
+
+Name , Age , Gender = "Updallahi" , 22 , "Male"
+print(Name);print(Age);print(Gender)
+print(type(Name));print(type(Age));print(type(Gender))
+Name , Age , Gender = "Sumayo" , 20 , "Female"
+print(Name);print(Age);print(Gender)
+print(type(Name));print(type(Age));print(type(Gender))
+Name , Age , Gender = "Epdirahman" , 18 , "Male"
+print(Name);print(Age);print(Gender)
+print(type(Name));print(type(Age));print(type(Gender))
+Name , Age , Gender = "Yahye" , 16 , "Male"
+print(Name);print(Age);print(Gender)
+print(type(Name));print(type(Age));print(type(Gender))
+Name , Age , Gender = "Saynab" , 14 , "Female"
+print(Name);print(Age);print(Gender)
+print(type(Name));print(type(Age));print(type(Gender))
+Name , Age , Gender = "Yusuf" , 12 , "Male"
+print(Name);print(Age);print(Gender)
+print(type(Name));print(type(Age));print(type(Gender))
+Name , Age , Gender = "Salmo" , 10 , "Female"
+print(Name);print(Age);print(Gender)
+print(type(Name));print(type(Age));print(type(Gender))
+Name , Age , Gender = "Sukayno" , 8 , "Female"
+print(Name);print(Age);print(Gender)
+print(type(Name));print(type(Age));print(type(Gender))
+Name , Age , Gender = "Hafso" , 6 , "Female"
+print(Name);print(Age);print(Gender)
+print(type(Name));print(type(Age));print(type(Gender))
+Name , Age , Gender = "Asiyo" , 4 , "Female"
+print(Name);print(Age);print(Gender)
+print(type(Name));print(type(Age));print(type(Gender))
+Name , Age , Gender = "Asmaa" , 2 , "Female"
+print(Name);print(Age);print(Gender)
+print(type(Name));print(type(Age));print(type(Gender))
