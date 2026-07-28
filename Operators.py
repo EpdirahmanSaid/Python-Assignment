01e6908c24360ec71955f8f8520e4b3c3d01278f
EpdirahmanSaid <epdirahman00@gmail.com>
2026-07-28 12:24:40 +0300

    Operators ASS

---
+++ Operators.py
@@ -0,0 +1,135 @@
+#Arithmetic Operators
+
+x = 1; y = 2
+print(x + y)
+print(x - y)
+print(x * y)
+print(x / y)
+print(x // y)
+print(x % y)
+print(x ** y)
+
+x = 3; y = 4
+print(x + y)
+print(x - y)
+print(x * y)
+print(x / y)
+print(x // y)
+print(x % y)
+print(x ** y)
+
+x = 5; y = 6
+print(x + y)
+print(x - y)
+print(x * y)
+print(x / y)
+print(x // y)
+print(x % y)
+print(x ** y)
+
+x = 7; y = 8
+print(x + y)
+print(x - y)
+print(x * y)
+print(x / y)
+print(x // y)
+print(x % y)
+print(x ** y)
+
+x = 9; y = 10
+print(x + y)
+print(x - y)
+print(x * y)
+print(x / y)
+print(x // y)
+print(x % y)
+print(x ** y)
+
+#Comparison Operators == != > < >= <=
+
+x = 1; y = 2
+print(x == y)
+print(x != y)
+print(x > y)
+print(x < y)
+print(x >= y)
+print(x <= y)
+
+x = 3; y = 4
+print(x == y)
+print(x != y)
+print(x > y)
+print(x < y)
+print(x >= y)
+print(x <= y)
+
+x = 5; y = 6
+print(x == y)
+print(x != y)
+print(x > y)
+print(x < y)
+print(x >= y)
+print(x <= y)
+
+x = 7; y = 8
+print(x == y)
+print(x != y)
+print(x > y)
+print(x < y)
+print(x >= y)
+print(x <= y)
+
+x = 9; y = 10
+print(x == y)
+print(x != y)
+print(x > y)
+print(x < y)
+print(x >= y)
+print(x <= y)
+
+#Assignment Operators = += -= *= /= //= %= **=
+
+x = 1; x+=2
+print(x)
+
+x = 3; x -= 4
+print(x)
+
+x = 5; x *= 6
+print(x)
+
+x = 7; x /= 8
+print(x)
+
+x = 9; x //= 10
+print(x)
+
+x = 11; x **= 12
+print(x)
+
+#Logical Comparison  Or And Not 
+
+x = 1; y = 2
+print(not(x == y))
+print(x == y or x != y)
+print(x == y and x != y)
+
+x = 3; y = 4
+print(not(x == y or x != y))
+print(x == y or x != y)
+print(x == y and x != y)
+
+x = 5; y = 6
+print(not(x == y or x != y))
+print(x == y or x != y)
+print(x == y and x != y)
+
+x = 7; y = 8
+print(not(x == y or x != y))
+print(x == y or x != y)
+print(x == y and x != y)
+
+x = 9; y = 10
+print(not(x == y or x != y))
+print(x == y or x != y)
+print(x == y and x != y)
