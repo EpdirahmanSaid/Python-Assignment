563408812c0a95bd3281b53a9ec9cc48ead57212
EpdirahmanSaid <epdirahman00@gmail.com>
2026-07-31 22:07:40 +0300

    Conditions ASS

---
+++ Conditions.py
@@ -0,0 +1,87 @@
+Name = input("Enter Your Name: "); Age = int(input("Enter Your Age: "))
+if Name == "Abdullahi" and Age == 22:
+    print("Your Name Abdullahi And Your Age 22")
+elif Name == "Sumayo" and Age == 22:
+    print("Your Name Is Not Correct")
+else :
+    print("Your Name Or Age Is Incorrect")
+
+Name = input("Enter Your Name: "); Age = int(input("Enter Your Age: "))
+if Name == "Sumayo" and Age == 20:
+    print("Your Name Sumayo And Your Age 20")
+elif Name == "Abdirahman" and Age == 20:
+    print("Your Name Is Not Correct")
+else:
+    print("Your Name Or Age Is Incorrect")
+
+Name = input("Enter Your Name: "); Age = int(input("Enter Your Age: "))
+if Name == "Abdirahman" and Age == 18:
+    print("Your Name Abdirahman And Your Age 18")
+elif Name == "Yahye" and Age == 18:
+    print("Your Name Is Not Correct")
+else:
+    print("Your Name Or Age Is Incorrect")
+
+Name = input("Enter Your Name: "); Age = int(input("Enter Your Age: "))
+if Name == "Yahye" and Age == 16:
+    print("Your Name Yahye And Your Age 16")
+elif Name != "Yahye" and Age == 16:
+    print("Your Name Is Not Correct")
+else:
+    print("Your Name Or Your Age Is Incorrect")
+
+Name = input("Enter Your Name: "); Age = int(input("Enter Your Age: "))
+if Name == "Saynab" and Age == 14:
+    print(" Your Name Is Saynab And Your Age 14")
+elif Name != "Saynab" and Age == 14:
+    print("Your Name Is Not Correct")
+else:
+    print("Your Name Or Age Is Incorrect")
+
+Name = input("Enter Your Name: "); Age = int(input("Enter Your Age: "))
+if Name == "Yusuf" and Age == 12:
+    print("Your Name Is Yusuf And Your Age 12")
+elif Name != "YUsuf" and Age == 12:
+    print("Your Name Is Not Correct")
+else:
+    print("Your Name Or Age Is Incorrect")
+
+Name = input("Enter Your Name: "); Age = int(input("Enter Your Age: "))
+if Name == "Salmo" and Age == 10:
+    print("Your Name Is Salmo And Your Age Is 10") 
+elif Name != "Salmo" and Age == 10:
+    print("Your Name Is Not Correct")
+else:
+    print("Your Name Or Age Is Incorrect")
+
+Name = input("Enter Your Name: "); Age = int(input("Enter Your Age; "))
+if Name == "Sukayno" and Age == 8:
+    print("Your Name Is Sukayno And Your Age Is 8")
+elif Name != "Sukayno" and Age == 8:
+    print("Your Name Is Not Correct")
+else:
+    print("Your Name Or Age Is Incorrect")
+
+Name = input("Enter Your Name: "); Age = int(input("Enter Your Age; "))
+if Name == "Hafso" and Age == 6:
+    print("Your Name Is Hafso And Your Age Is 6")
+elif Name != "Hafso" and Age == 6:
+    print("Your Name Is Not Correct")
+else:
+    print("Your Name Or Age Is Incorrect")
+
+Name = input("Enter Your Name: "); Age = int(input("Enter Your Age; "))
+if Name == "Aasiyo" and Age == 4:
+    print("Your Name Is Aasiyo And Your Age Is 4")
+elif Name != "Aasiyo" and Age == 4:
+    print("Your Name Is Not Correct")
+else:
+    print("Your Name Or Age Is Incorrect")
+
+Name = input("Enter Your Name: "); Age = int(input("Enter Your Age; "))
+if Name == "Asmaa" and Age == 2:
+    print("Your Name Is Asmaa And Your Age Is 2")
+elif Name != "Asmaa" and Age == 2:
+    print("Your Name Is Not Correct")
+else:
+    print("Your Name Or Age Is Incorrect")
