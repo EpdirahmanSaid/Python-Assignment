76552b971e991357624dd994d529501f47e74093
EpdirahmanSaid <epdirahman00@gmail.com>
2026-08-06 13:38:16 +0300

    Loops ASS

---
+++ Loops.py
@@ -0,0 +1,121 @@
+#================================== WHILE LOOP =============================
+
+Name = input("Enter Your Name: "); Age = input("Enter Your Age: "); Gender = input("Enter Your Gender: ")
+x = 1
+while x <= 10:
+    print("Your Name Is: " + Name,"Your Age Is: " + Age,"Your Gender Is: " + Gender)
+    x += 1
+
+Name = input("Enter Your Name: "); Age = input("Enter Your Age: "); Gender = input("Enter Your Gender: ")
+x = 1
+while x <= 10:
+    print("Your Name Is: " + Name,"Your Age Is: " + Age,"Your Gender Is: " + Gender)
+    x += 1
+
+Name = input("Enter Your Name: "); Age = input("Enter Your Age: "); Gender = input("Enter Your Gender: ")
+x = 1
+while x <= 10:
+    print("Your Name Is: " + Name, "Your Age Is: " + Age, "Your Gender Is: " + Gender)
+    x += 1
+
+Name = input("Enter Your Name: "); Age = input("Enter Your Age: "); Gender = input("Enter Your Gender: ")
+x = 1
+while x <= 10:
+    print("Your Name Is: " + Name, "Your Age Is: " + Age, "Your Gender Is: " + Gender)
+    x += 1
+
+Name = input("Enter Your Name: "); Age = input("Enter Your Age: "); Gender = input("Enter Your Gender: ")
+x = 1
+while x <= 10:
+    print("Your Name Is: " + Name, "Your Age Is: " + Age, "Your Gender Is: " + Gender)
+    x += 1
+
+Name = input("Enter Your Name: "); Age = input("Enter Your Age: "); Gender = input("Enter Your Gender: ")
+x = 1
+while x <= 10:
+    print("Your Name Is: " + Name, "Your Age Is: " + Age, "Your Gender Is; " + Gender)
+    x += 1
+
+Name = input("Enter Your Name: "); Age = input("Enter Your Age: "); Gender = input("Enter Your Gender: ")
+x = 1
+while x <= 10:
+    print("Your Name Is: " + Name, "Your Age Is: " + Age, "Your Gender Is: " + Gender)
+    x += 1
+
+Name = input("Enter Your Name: "); Age = input("Enter Your Age: "); Gender = input("Enter Your Gender: ")
+x = 1
+while x <= 10:
+    print("Your Name Is: " + Name, "Your Age Is: " + Age, "Your Gender Is: " + Gender)
+    x += 1
+
+Name = input("Enter Your Name: "); Age = input("Enter Your Age: "); Gender = input("Enter Your Gender: ")
+x = 1
+while x <= 10:
+    print("Your Name Is: " + Name, "Your Age Is: " + Age, "Your Gender Is: " + Gender)
+    x += 1
+
+Name = input("Enter Your Name: "); Age = input("Enter Your Age: "); Gender = input("Enter Your Gender: ")
+x = 1
+while x <= 10:
+    print("Your Name Is: " + Name, "Your Age Is: " + Age, "Your Gender Is: " + Gender)
+    x += 1
+
+Name = input("Enter Your Name: "); Age = input("Enter Your Age: "); Gender = input("Enter Your Gender: ")
+x = 1
+while x <= 10:
+    print("Your Name Is: " + Name, "Your Age Is: " + Age, "Your Gender Is: " + Gender)
+    x += 1
+
+#=====================================FOR LOOP=========================================
+
+Name = input("Enter Your Name: "); Age = input("Enter Your Age: "); Gender = input("Enter Your Gender: ")
+for x in range (10):
+    print("Your Name Is: " + Name, "Your Age Is: " + Age, "Your Gender Is: " + Gender)
+
+Name = input("Enter your Name: "); Age = input("Enter Your Age: "); Gender = input("Enter Your Gender: ")
+for x in range (10):
+    print("Your Name Is: " + Name, "Your Age Is: " + Age, "Your Gender Is: " + Gender)
+
+Name = input("Enter Your Name: "); Age = input("Enter Your Age: "); Gender = input("Enter Your Gender: ")
+for x in range (10):
+    print("Your Name Is: " + Name, "Your Age Is: " + Age, "Your Gender Is: " + Gender)
+
+Name = input("Enter Your Name: "); Age = input("Enter Your Age: "); Gender = input("Enter Your Gender: ")
+for x in range (10):
+    print("Your Name Is: " + Name, "Your Age Is: " + Age, "Your Gender Is: " + Gender)
+
+Name = input("Enter Your Name: "); Age = input("Enter Your Age: "); Gender = input("Enter Your Gender: ")
+for x in range (10):
+    print("Your Name Is: " + Name, "Your Age Is: " + Age, "Your Gender Is: " + Gender)
+
+Name = input("Enter Your Name: "); Age = input("Enter Your Age: "); Gender = input("Enter Your Gender: ")
+for x in range (10):
+    print("Your Name Is: " + Name, "Your Age Is: " + Age, "Your Gender Is: " + Gender)
+
+Name = input("Enter Your Name: "); Age = input("Enter Your Age: "); Gender = input("Enter Your Gender: ")
+for x in range (10):
+    print("Your Name Is: " + Name, "Your Age Is: " + Age, "Your Gender Is: " + Gender)
+
+Name = input("Enter Your Name: "); Age = input("Enter Your Age: "); Gender = input("Enter Your Gender: ")
+for x in range (10):
+    print("Your Name Is: " + Name, "Your Age Is: " + Age, "Your Gender Is: " + Gender)
+
+Name = input("Enter Your Name: "); Age = input("Enter Your Age: "); Gender = input("Enter Your Gender: ")
+for x in range (10):
+    print("Your Name Is: " + Name, "Your Age Is: " + Age, "Your Gender Is: " + Gender)
+
+Name = input("Enter Your Name: "); Age = input("Enter Your Age: "); Gender = input("Enter Your Gender: ")
+for x in range (10):
+    print("Your Name Is: " + Name, "Your Age Is: " + Age, "Your Gender Is: " + Gender)
+
+Name = input("Enter Your Name: "); Age = input("Enter Your Age: "); Gender = input("Enter Your Gender: ")
+for x in range (10):
+    print("Your Name Is: " + Name, "Your Age Is: " + Age, "Your Gender Is: " + Gender)
+
+
+
+
+
+
+
+
